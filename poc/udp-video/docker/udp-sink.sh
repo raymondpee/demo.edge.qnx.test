5ff70d0e6427508c8b795f892e96ee3fa96e3d24
Raymond Pee Yong Nging <raymondpyn@gmail.com>
2024-01-22 23:05:58 +0800

    Include rtsp example

--- poc/udp-video/docker/udp-sink.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export DISPLAY=:1.0
-xhost +
-docker run -it --network=host --rm -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix raymondpyn/video-udpsink:19122023
--- poc/udp-video/docker/udp-src.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -it --network=host -e UDP_ADDR=192.168.18.76 raymondpyn/video-udpsrc:27122023
